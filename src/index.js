89042585e7b5f27f0cdc42a2bc3bd9644928d09b
Erskine Gamaliel L. Duenas <erskine.duenas@gmail.com>
2022-09-06 02:44:57 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,3 +6,4 @@ ReactDOM.render(<h1>My Favourite Foods</h1>, document.getElementById("root"));
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+AudioWorklet
